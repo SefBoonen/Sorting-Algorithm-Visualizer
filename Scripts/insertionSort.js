ad222fdd21abcdd5341b7ad3e940de4393bfb5dc
SefBoonen <1boonen.sef1@gmail.com>
2023-08-05 16:23:47 +0200

    Added insertion sort not fully complete

--- Scripts/insertionSort.js
+++ Scripts/insertionSort.js
@@ -1,3 +1,25 @@
 async function insertionSort(delay) {
-    
+    let sorted = [];
+    for(i = 0; i < lines.length; i++) {
+        
+        if(lines[i] > lines[i + 1]) {
+            let swap = lines[i];
+            lines[i] = lines[i + 1];
+            lines[i + 1] = swap;
+            sorted.push(lines[i]);
+        } else {
+            sorted.push(lines[i]);
+        }
+
+        for(j = sorted.length; j > 0; j--) {
+            if(sorted[j] < sorted[j - 1]) {
+                let swap = sorted[j];
+                sorted[j] = sorted[j - 1];
+                sorted[j - 1] = swap;
+            } 
+            setLinesToContainer(sorted);
+        } 
+        await wait(delay);
+        
+    }
 }
