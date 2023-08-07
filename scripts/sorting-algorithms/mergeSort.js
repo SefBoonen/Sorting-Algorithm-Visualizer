8290de33f95391b4db3971f73f6b249d313f9110
SefBoonen <1boonen.sef1@gmail.com>
2023-08-07 12:42:53 +0200

    Made the merge function

--- scripts/sorting-algorithms/mergeSort.js
+++ scripts/sorting-algorithms/mergeSort.js
@@ -1,3 +1,23 @@
 async function mergeSort(delay) {
-    
+    console.log(merge(lines, lines1));
+}
+
+function merge(arr1, arr2) {
+    let sorted = [];
+
+    while(arr1.length > 0 && arr2.length > 0) {
+        if(arr1[0] < arr2[0]) {
+            sorted.push(arr1.shift());
+        } else if (arr2[0] < arr1[0]) {
+            sorted.push(arr2.shift());
+        }
+    }
+
+    if(arr1.length < 1) {
+        sorted = sorted.concat(arr2)
+    } else if (arr2.length < 1) {
+        sorted = sorted.concat(arr1)
+    }
+
+    return sorted;
 }
