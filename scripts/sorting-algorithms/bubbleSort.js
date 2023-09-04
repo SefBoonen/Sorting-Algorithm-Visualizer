684d9ad2015224a03ba839925ea5d82b3ad4f6bc
SefBoonen <1boonen.sef1@gmail.com>
2023-09-04 11:00:02 +0200

    Fixed bubble sort not doing check

--- scripts/sorting-algorithms/bubbleSort.js
+++ scripts/sorting-algorithms/bubbleSort.js
@@ -2,17 +2,17 @@ async function bubbleSort(delay) {
     for (i = lines.length - 1; i >= 0; i--) {
         let swapped = false;
         for (j = 0; j < i; j++) {
+            document.getElementById(`l${lines[j]}`).className = "selected";
+            document.getElementById(`l${lines[j + 1]}`).className = "selected";
+            await wait(delay);
             if (stop) {
                 return;
             }
             if (lines[j] > lines[j + 1]) {
-                document.getElementById(`l${lines[j]}`).className = "selected";
-                document.getElementById(`l${lines[j + 1]}`).className = "selected";
-                await wait(delay);
                 [lines[j], lines[j + 1]] = [lines[j + 1], lines[j]];
                 swapped = true;
-                setLinesToContainer(lines);
             }
+            setLinesToContainer(lines);
         }
         if (!swapped) {
             return;
